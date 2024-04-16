5a1d270c9a73c04f2e852f604ae8bab21011a03e
unknown <DAM103@iesataulfo.local>
2024-04-16 15:36:35 +0200

    Cuarta fase proyecto (Clase1 codificada y comentada y Principal creadas y comentadas)

--- src/directorioTrabajoArredondoGuillermo/Clase.java
+++ src/directorioTrabajoArredondoGuillermo/Clase.java
@@ -4,5 +4,11 @@ package directorioTrabajoArredondoGuillermo;
  * Esta clase va a contener un metodo el cual va a mostrar por pantalla un mensaje para reproducirlo en Principal
  */
 public class Clase {
+	
+	
+	//Metodo para mostrar por pantalla el siguiente mensaje.
+	public void clase() {
+		System.out.println("Soy un objeto de la clase y acabo de ser creado");
+	}
 
 }
