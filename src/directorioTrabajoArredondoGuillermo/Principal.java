42e1bf7b9aea857eb30eaebe193abe72be2574ad
unknown <DAM103@iesataulfo.local>
2024-04-16 15:31:08 +0200

    Segunda fase proyecto (Clase1 creada y Principal creada y comentada)

--- src/directorioTrabajoArredondoGuillermo/Principal.java
+++ src/directorioTrabajoArredondoGuillermo/Principal.java
@@ -3,7 +3,7 @@ package directorioTrabajoArredondoGuillermo;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//La funcion de esta clase es mostrar por pantalla el objeto creado en la clase Clase.
 
 	}
 
