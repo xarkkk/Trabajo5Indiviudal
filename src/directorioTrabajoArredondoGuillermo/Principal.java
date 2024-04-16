77975c58a79882fce3f428d67ed536b80750d24a
unknown <DAM103@iesataulfo.local>
2024-04-16 15:42:42 +0200

    Quinta fase proyecto

--- src/directorioTrabajoArredondoGuillermo/Principal.java
+++ src/directorioTrabajoArredondoGuillermo/Principal.java
@@ -4,6 +4,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		//La funcion de esta clase es mostrar por pantalla el objeto creado en la clase Clase.
+		
+		//Objeto del tipo clase llamado clase1
+		Clase clase1=new Clase();
+		
+		
+		//Llamada el metodo clase para el objeto clase1
+		clase1.clase();
 
 	}
 
