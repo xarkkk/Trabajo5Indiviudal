ddaee8d3e4be467a9722759eaf18360dfd0099f7
unknown <DAM103@iesataulfo.local>
2024-04-16 15:33:13 +0200

    Tercera fase proyecto (Clase1 y Principal creadas y comentadas)

--- src/directorioTrabajoArredondoGuillermo/Clase.java
+++ src/directorioTrabajoArredondoGuillermo/Clase.java
@@ -1,5 +1,8 @@
 package directorioTrabajoArredondoGuillermo;
 
+/*
+ * Esta clase va a contener un metodo el cual va a mostrar por pantalla un mensaje para reproducirlo en Principal
+ */
 public class Clase {
 
 }
